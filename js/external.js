12502aa418f67c185a8e39acadace0f9ffbc9f49
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2019-06-01 19:57:54 -0400

    Cleaned up code a bit, created clearer functions.

--- js/external.js
+++ js/external.js
@@ -1,43 +1,43 @@
-function setJsonLocalStorage(str) {
-    //localStorage.setItem("contents", JSON.stringify(json));
-    localStorage.setItem("contents", str);
+function setLocalStorage(json) {
+    localStorage.setItem("contents", JSON.stringify(json));
+    // localStorage.setItem("contents", str);
 }
 
-function getJsonLocalStorage() {
-    let json = JSON.parse(localStorage.getItem("contents"));
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem("contents"));
+}
 
-    clearFolders();
+function constructFolders() {
+  clearFolders();
+  const json = getLocalStorage();
 
-    for (let category in json) {
-        let div = document.createElement("div");
-        div.className = "folder";
+  for (const category in json) {
+      const div = document.createElement("div");
+      div.className = "folder";
+      document.getElementById('folders').appendChild(div);
 
-        let title = document.createElement("h6");
-        title.innerHTML = category;
+      const title = document.createElement("h6");
+      title.innerHTML = category;
+      div.appendChild(title);
+      div.appendChild(document.createElement('br'));
 
-        let ul = document.createElement("ul");
+      const ul = document.createElement("ul");
+      div.appendChild(ul);
 
-        for (let linkName in json[category].links) {
-            let li = document.createElement("ul");
+      for (const linkName in json[category].links) {
+          const li = document.createElement("ul");
+          ul.appendChild(li);
 
-            let hyperLink = document.createElement("a");
-            hyperLink.textContent = linkName;
-            hyperLink.href = json[category].links[linkName];
+          const hyperLink = document.createElement("a");
+          hyperLink.textContent = linkName;
+          hyperLink.href = json[category].links[linkName];
+          li.appendChild(hyperLink);
+      }
 
-            li.appendChild(hyperLink);
-            ul.appendChild(li);
-        }
+  }
 
-        div.appendChild(title);
-        div.appendChild(document.createElement('br'));
-        div.appendChild(ul);
-        document.getElementById("folders").appendChild(div);
-    }
-
-    return localStorage.getItem("contents");
 }
 
 function clearFolders() {
-    let currentFolders = document.getElementById("folders");
-    currentFolders.innerHTML = "";
+    document.getElementById('folders').innerHTML = '';
 }
