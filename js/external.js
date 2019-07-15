7426aa934cf42012fc09f6b78b0d18e693322e04
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2019-07-15 18:12:16 -0400

    Now uses obj destructuring to parse JSON in cleaner fashion

--- js/external.js
+++ js/external.js
@@ -1,36 +1,38 @@
+const localStorageVar = "contents";
+
 function setLocalStorage(json) {
-    localStorage.setItem("contents", JSON.stringify(json));
+    localStorage.setItem(localStorageVar, JSON.stringify(json));
     // localStorage.setItem("contents", str);
 }
 
 function getLocalStorage() {
-    return JSON.parse(localStorage.getItem("contents"));
+    return JSON.parse(localStorage.getItem(localStorageVar));
 }
 
 function constructFolders() {
   clearFolders();
   const json = getLocalStorage();
 
-  for (const category in json) {
+  for (let [catTitle, catContents] of Object.entries(json)) {
       const div = document.createElement("div");
       div.className = "folder";
       document.getElementById('folders').appendChild(div);
 
       const title = document.createElement("h6");
-      title.innerHTML = category;
+      title.innerHTML = catTitle;
       div.appendChild(title);
       div.appendChild(document.createElement('br'));
 
       const ul = document.createElement("ul");
       div.appendChild(ul);
 
-      for (const linkName in json[category].links) {
+      for (let [linkTitle, linkURL] of Object.entries(catContents.links)) {
           const li = document.createElement("ul");
           ul.appendChild(li);
 
           const hyperLink = document.createElement("a");
-          hyperLink.textContent = linkName;
-          hyperLink.href = json[category].links[linkName];
+          hyperLink.textContent = linkTitle;
+          hyperLink.href = linkURL;
           li.appendChild(hyperLink);
       }
 
