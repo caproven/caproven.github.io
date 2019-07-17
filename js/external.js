b331aa904aa963c5ed90eac92a24214cf38cae1d
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2019-07-17 17:12:06 -0400

    Cleaned up code and added folder to construct JSON from HTML page

--- js/external.js
+++ js/external.js
@@ -1,45 +1,76 @@
-const localStorageVar = "contents";
+const localStorageVar = 'contents';
 
+/**
+ * Saves to local storage the stringified JSON representing user's bookmarks.
+ */
 function setLocalStorage(json) {
-    localStorage.setItem(localStorageVar, JSON.stringify(json));
-    // localStorage.setItem("contents", str);
+  localStorage.setItem(localStorageVar, JSON.stringify(json));
 }
 
+/**
+ * Retrieves the JSON representing user's bookmarks from local storage.
+ */
 function getLocalStorage() {
-    return JSON.parse(localStorage.getItem(localStorageVar));
+  return JSON.parse(localStorage.getItem(localStorageVar));
 }
 
+/**
+ * Populates the HTML page with contents from user's bookmarks.
+ */
 function constructFolders() {
   clearFolders();
   const json = getLocalStorage();
 
   for (let [catTitle, catContents] of Object.entries(json)) {
-      const div = document.createElement("div");
-      div.className = "folder";
-      document.getElementById('folders').appendChild(div);
+    const div = document.createElement('div');
+    div.className = 'folder';
+    document.getElementById('folders').appendChild(div);
 
-      const title = document.createElement("h6");
-      title.innerHTML = catTitle;
-      div.appendChild(title);
-      div.appendChild(document.createElement('br'));
+    const title = document.createElement('h6');
+    title.className = 'title';
+    title.innerHTML = catTitle;
+    div.appendChild(title);
+    div.appendChild(document.createElement('br'));
 
-      const ul = document.createElement("ul");
-      div.appendChild(ul);
+    const ul = document.createElement('ul');
+    ul.className = 'bookmarks';
+    div.appendChild(ul);
 
-      for (let [linkTitle, linkURL] of Object.entries(catContents.links)) {
-          const li = document.createElement("ul");
-          ul.appendChild(li);
+    for (let [linkTitle, linkURL] of Object.entries(catContents.links)) {
+      const li = document.createElement('li');
+      ul.appendChild(li);
 
-          const hyperLink = document.createElement("a");
-          hyperLink.textContent = linkTitle;
-          hyperLink.href = linkURL;
-          li.appendChild(hyperLink);
-      }
-
+      const hyperLink = document.createElement('a');
+      hyperLink.textContent = linkTitle;
+      hyperLink.href = linkURL;
+      li.appendChild(hyperLink);
+    }
   }
-
 }
 
+/**
+ * Parse the current HTML page for user's bookmarks, returning them as in JSON.
+ */
+function foldersToJson() {
+  let json = {};
+
+  const foldersDOM = document.getElementsByClassName('folder');
+  for (let folder of foldersDOM) {
+    const title = folder.firstChild.innerText;
+    json[title] = {};
+
+    const linksDOM = folder.lastChild;
+    for (let link of linksDOM.childNodes) {
+      json[title][link.firstChild.innerText] = link.firstChild.href;
+    }
+  }
+
+  return json;
+}
+
+/**
+ * Clears the HTML page of user's bookmarks.
+ */
 function clearFolders() {
-    document.getElementById('folders').innerHTML = '';
+  document.getElementById('folders').innerHTML = '';
 }
